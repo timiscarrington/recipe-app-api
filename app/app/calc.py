f0819751a189bc2e021aa5b80dfd2f39b830a7ec
timiscarrington <timiscarrington@gmail.com>
2023-05-29 22:59:46 -0700

    forgot to add psycopg2 version to requirments.txt, just a piggy back on last commit

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator Functions
+"""
+
+def add(x, y):
+    """add x and y and return result"""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return result"""
+    return x - y
