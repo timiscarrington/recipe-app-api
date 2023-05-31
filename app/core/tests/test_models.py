d14481502b1c6f48532bf26ffaf733fa87dad0f5
timiscarrington <timiscarrington@gmail.com>
2023-05-31 12:32:55 -0700

    added test and ran test to ensure emails would be normalized

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -24,8 +24,8 @@ class ModelTests(TestCase):
         """Test email is normalized for new users."""
         sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
-            ['Test1@Example.COM', 'Test2@example.com'],
-            ['TEST3@example.COM', 'TEST3@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
             ['test4@example.COM', 'test4@example.com'],
         ]
         for email, expected in sample_emails:
