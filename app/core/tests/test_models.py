79e5f36ed0980491d1833635c5fcb8fa786a0b1b
timiscarrington <timiscarrington@gmail.com>
2023-05-31 12:29:42 -0700

    update version

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,6 +7,7 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
     """Test models"""
 
+
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful"""
         email = 'test@example.com'
@@ -18,3 +19,15 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users."""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test1@Example.COM', 'Test2@example.com'],
+            ['TEST3@example.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
