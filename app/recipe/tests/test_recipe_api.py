1e67e3d661bae197928f7b9b36f0c5849c32a379
timiscarrington <timiscarrington@gmail.com>
2023-06-07 16:58:42 -0700

    added recipe app, added tests folder, wrote tests for retrieving auth/unauth recipes

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -15,7 +15,7 @@ from core.models import Recipe
 from recipe.serializers import RecipeSerializer
 
 
-RECIPES_URL = reverse('recipes:recipe-list')
+RECIPES_URL = reverse('recipe:recipe-list')
 
 def create_recipe(user, **params):
     """Create and return a sample recipe"""
@@ -24,7 +24,7 @@ def create_recipe(user, **params):
         'time_minutes': 22,
         'price': Decimal('5.25'),
         'description': 'sample description',
-        'link': 'https://example.com/recipe.pdf',
+        'link': 'http://example.com/recipe.pdf',
     }
     defaults.update(params)
 
@@ -66,13 +66,13 @@ class PrivateRecipeApiTests(TestCase):
 
             recipes = Recipe.objects.all().order_by('id')
             serializer = RecipeSerializer(recipes, many=True)
-            self.assertEqual(res.status_code, status.HTTPS_200_OK)
+            self.assertEqual(res.status_code, status.HTTP_200_OK)
             self.assertEqual(res.data, serializer.data)
 
     def test_recipe_list_limited_to_user(self):
         """Test list of recipes is limited to authenticated user."""
         other_user = get_user_model().objects.create_user(
-            'otherr@example.com',
+            'other@example.com',
             'password123',
         )
         create_recipe(user=other_user)
