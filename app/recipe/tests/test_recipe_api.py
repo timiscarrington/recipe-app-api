f3acc42fe5924f0b1aa4e7861df0e5862ca2b69c
timiscarrington <timiscarrington@gmail.com>
2023-06-07 17:51:09 -0700

    updated serializer, and views to create recipes. ran tests. Test passed

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -107,7 +107,7 @@ class PrivateRecipeApiTests(TestCase):
     def test_create_recipe(self):
         """Test creating a recipe"""
         payload = {
-            'title': 'Sample Recipe',
+            'title': 'Sample recipe',
             'time_minutes': 30,
             'price': Decimal('5.99'),
         }
@@ -117,4 +117,4 @@ class PrivateRecipeApiTests(TestCase):
         recipe = Recipe.objects.get(id=res.data['id'])
         for k, v in payload.items():
             self.assertEqual(getattr(recipe, k), v)
-            self.assertEqual(recipe.user, self.user)
+        self.assertEqual(recipe.user, self.user)
