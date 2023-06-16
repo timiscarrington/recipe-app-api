f43bbe136f40b37ce852e31ce73e4c449d12f7ff
timiscarrington <timiscarrington@gmail.com>
2023-06-16 09:47:54 -0700

    addedd mixin to update in views, test written to test updating ingredients. Test passed

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -39,38 +39,38 @@ class PublicIngredientsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-    class PrivateIngredientsApiTests(TestCase):
-        """Test anauthenticated API requests"""
+class PrivateIngredientsApiTests(TestCase):
+    """Test anauthenticated API requests"""
 
-        def setUp(self):
-            self.user = create_user()
-            self.client = APIClient()
-            self.client.force_authenticate(self.user)
+    def setUp(self):
+        self.user = create_user()
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
 
-        def test_retrieve_ingredients(self):
-            """Test retrieving a list of ingredients"""
-            Ingredient.objects.create(user=self.user, name='Spinach')
-            Ingredient.objects.create(user=self.user, name='Choclate')
+    def test_retrieve_ingredients(self):
+        """Test retrieving a list of ingredients"""
+        Ingredient.objects.create(user=self.user, name='Spinach')
+        Ingredient.objects.create(user=self.user, name='Choclate')
 
-            res = self.client.get(INGREDIENTS_URL)
+        res = self.client.get(INGREDIENTS_URL)
 
-            Ingredients = Ingredient.objects.all().order_by('-name')
-            serializer = IngredientSerializer(ingredients, many=True)
-            self.assertEqual(res.status_code, status.HTTP_200_OK)
-            self.assertEqual(res.data, serializer.data)
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(ingredients, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
 
-        def test_ingredients_limited_to_user(self):
-            """"Test list of ingredients is limited to authenticated user"""
-            user2= create_user(email='user2@example.com')
-            Ingredient.objects.create(user=user2, name='Salt')
-            ingredient = Ingredient.objects.create(user=self.user, name='Pepper')
+    def test_ingredients_limited_to_user(self):
+        """"Test list of ingredients is limited to authenticated user"""
+        user2= create_user(email='user2@example.com')
+        Ingredient.objects.create(user=user2, name='Salt')
+        ingredient = Ingredient.objects.create(user=self.user, name='Pepper')
 
-            res = self.client.get(INGREDIENTS_URL)
+        res = self.client.get(INGREDIENTS_URL)
 
-            self.assertEqual(res.status_code, status.HTTP_200_OK)
-            self.assertEqual(len(res.data), 1)
-            self.assertEqual(res.data[0]['name'], ingredient.name)
-            self.assertEqual(res.data[0]['id'], ingredient.id)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], ingredient.name)
+        self.assertEqual(res.data[0]['id'], ingredient.id)
 
     def test_update_ingredient(self):
         """Test updating ingredient"""
